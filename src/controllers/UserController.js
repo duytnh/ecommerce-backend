1282267f89eacee00104aec8fb3ce8f8c511588f
duytnh <duytnh201@gmail.com>
2023-09-14 11:14:17 -0700

    ok

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -168,7 +168,7 @@ const refreshToken = async (req, res) => {
 const logoutUser = async (req, res) => {
     try {
         res.clearCookie('refresh_token')
-        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('refresh_token')
         return res.status(200).json({
             status: 'OK',
             message: 'Logout successfully'
